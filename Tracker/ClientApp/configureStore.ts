fa9105357b62e1c9556eee77889d84064cbf967c
Anthony Williams <anthony.williams.cs@gmail.com>
2018-07-20 16:25:11 -0600

    Applied tslint code formatting across entire ClientApp

--- Tracker/ClientApp/configureStore.ts
+++ Tracker/ClientApp/configureStore.ts
@@ -11,23 +11,26 @@ import {
 } from 'redux'
 import thunk from 'redux-thunk'
 import * as StoreModule from './store'
-import { ApplicationState, reducers } from './store'
+import { IApplicationState, reducers } from './store'
 
 import { History } from 'history'
 
-export default function configureStore(history: History, initialState?: ApplicationState) {
+export default function configureStore(history: History, initialState?: IApplicationState) {
 	// Build middleware. These are functions that can process the actions before they reach the store.
 	const windowIfDefined = typeof window === 'undefined' ? null : window as any
 	// If devTools is installed, connect to it
 	const devToolsExtension = windowIfDefined && windowIfDefined.__REDUX_DEVTOOLS_EXTENSION__ as () => GenericStoreEnhancer
-	const createStoreWithMiddleware = compose(
-		applyMiddleware(thunk, routerMiddleware(history)),
+	const createStoreWithMiddleware: any = compose(
+		applyMiddleware(
+			thunk,
+			routerMiddleware(history)
+		),
 		devToolsExtension ? devToolsExtension() : <S>(next: StoreEnhancerStoreCreator<S>) => next
 	)(createStore)
 
 	// Combine all reducers and instantiate the app-wide store instance
 	const allReducers = buildRootReducer(reducers)
-	const store = createStoreWithMiddleware(allReducers, initialState) as Store<ApplicationState>
+	const store = createStoreWithMiddleware(allReducers, initialState) as Store<IApplicationState>
 
 	// Enable Webpack hot module replacement for reducers
 	if (module.hot) {
@@ -41,5 +44,5 @@ export default function configureStore(history: History, initialState?: Applicat
 }
 
 function buildRootReducer(allReducers: ReducersMapObject) {
-	return combineReducers<ApplicationState>(Object.assign({}, allReducers, { routing: routerReducer }))
+	return combineReducers<IApplicationState>(Object.assign({}, allReducers, { routing: routerReducer }))
 }
--- Tracker/ClientApp/routes.tsx
+++ Tracker/ClientApp/routes.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import { Route } from 'react-router-dom'
+import { Route, RouteComponentProps } from 'react-router-dom'
 import { Layout } from './components/Layout'
 
 import Counter from './components/Counter'
@@ -8,6 +8,6 @@ import Home from './components/Home'
 
 export const routes = <Layout>
 	<Route exact path='/' component={Home} />
-	<Route path='/counter' component={Counter} />
-	<Route path='/fetchdata/:startDateIndex?' component={FetchData} />
+	<Route path='/counter' component={Counter as any} />
+	<Route path='/fetchdata/:startDateIndex?' component={FetchData as any} />
 </Layout>
